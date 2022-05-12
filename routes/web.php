9a9ef837ba2e993e8bf214c7b40e8be807805785
loubal70 <louis.boulanger.work@gmail.com>
2022-05-12 18:01:17 +0200

    Condition add/remove admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GoogleController;
 use App\Http\Controllers\PointerController;
@@ -39,12 +40,59 @@ Route::fallback(function () {
 
 Route::middleware(['auth:sanctum', 'role:admin'])->group(function(){
     Route::get('/addadmin/{id}', function ($id) {
-        $user = User::find($id);
-        $user->roles()->attach('1');
+
+        if(Auth::id() != $id){
+            $user = User::find($id);
+            // Est-ce que l'utilisateur existe
+            if( !empty($user) ){
+                // Il existe, est-ce que l'utilisateur n'est pas déjà admin ?
+                if( empty($user->roles()->where('role_id', '1')->first()) ){
+                    $user->roles()->attach('1');
+                    return redirect()->route('dashboard')->with('message', "$user->prenom est maintenant administrateur");
+                }
+                else{
+                    // L'utilisateur était déjà admin
+                    return redirect()->route('dashboard')->with('message', "$user->prenom est déjà un administrateur");
+                }
+
+            }
+            else{
+                // L'utilisateur n'existe pas
+                return redirect()->back()->with('message', "L'utilisateur n'existe pas");
+            }
+
+        }
+        else{
+            return redirect()->route('dashboard')->with('message', 'Vous ne pouvez pas toucher à vos propres permissions');
+        }
     });
 
     Route::get('/removeadmin/{id}', function ($id) {
-        $user = User::find($id);
-        $user->roles()->attach('1');
+
+        if(Auth::id() != $id){
+            $user = User::find($id);
+            // Est-ce que l'utilisateur existe
+            if( !empty($user) ){
+                // Il existe, est-ce que l'utilisateur est admin ?
+                if( !empty($user->roles()->where('role_id', '1')->first()) ){
+                    $user->roles()->detach('1');
+                    return redirect()->route('dashboard')->with('message', "$user->prenom n'est plus administrateur");
+                }
+                else{
+                    // L'utilisateur n'est pas admin
+                    return redirect()->route('dashboard')->with('message', "$user->prenom n'est déjà pas un administrateur");
+                }
+
+            }
+            else{
+                // L'utilisateur n'existe pas
+                return redirect()->back()->with('message', "L'utilisateur n'existe pas");
+            }
+
+        }
+        else{
+            return redirect()->route('dashboard')->with('message', 'Vous ne pouvez pas toucher à vos propres permissions');
+        }
+        
     });
 });
