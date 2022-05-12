6840385ee7aa19ada62f338742fabe6282520422
loubal70 <louis.boulanger.work@gmail.com>
2022-05-12 18:51:20 +0200

    CustomErrorMessageRegister

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -4,11 +4,13 @@ namespace App\Actions\Fortify;
 
 use App\Models\Team;
 use App\Models\User;
+use Laravel\Jetstream\Jetstream;
+use App\Mail\WelcomeMarkdownMail;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
-use Laravel\Jetstream\Jetstream;
 
 class CreateNewUser implements CreatesNewUsers
 {
@@ -25,10 +27,23 @@ class CreateNewUser implements CreatesNewUsers
         Validator::make($input, [
             'prenom'    => ['required', 'string', 'max:255'],
             'nom'       => ['required', 'string', 'max:255'],
-            'username'  => ['required', 'string', 'max:255'],
+            'username'  => ['required', 'unique:users', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
+        ],
+        [
+            'username.required'     =>  'Un nom d\'utilisateur est obligatoire',
+            'username.string'       =>  'Votre nom d\'utilisateur doit être une chaine de caractères',
+            'username.max'          =>  'Votre nom d\'utilisateur ne peut pas dépasser 255 caractères',
+            'username.unique'       =>  'Votre nom d\'utilisateur est déjà utilisé',
+
+            'email.required'        =>  'Un email est obligatoire',
+            'email.string'          =>  'Votre email doit être une chaine de caractères',
+            'email.email'           =>  'Votre email doit être une adresse email valide',
+            'email.max'             =>  'Votre email ne peut pas dépasser 255 caractères',
+            'email.unique'          =>  'Votre email est déjà utilisé',
+            
         ])->validate();
 
         return DB::transaction(function () use ($input) {
@@ -40,8 +55,10 @@ class CreateNewUser implements CreatesNewUsers
                 'password' => Hash::make($input['password']),
             ]), function (User $user) {
                 $this->createTeam($user);
+                Mail::to($user['email'])->send(new WelcomeMarkdownMail());
             });
         });
+
     }
 
     /**
