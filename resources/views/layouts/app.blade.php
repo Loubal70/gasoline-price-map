ad4263f3eadd4ab8d12ef1bc2741321f8cd0c2bb
loubal70 <louis.boulanger.work@gmail.com>
2022-05-14 18:27:15 +0200

    PWA Good

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,6 +61,7 @@
             if (!navigator.serviceWorker.controller) {
                 navigator.serviceWorker.register("/sw.js").then(function (reg) {
                     console.log("Service worker has been registered for scope: " + reg.scope);
+                    reg.update();
                 });
             }
             // if ('serviceWorker' in navigator) {
